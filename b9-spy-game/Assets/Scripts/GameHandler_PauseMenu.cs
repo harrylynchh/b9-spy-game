3ea3fdc895e525274345abefb6a3fff9aa86d42e
Harry Lynch <hlynch02@tufts.edu>
2025-02-25 17:44:40 -0500

    Assembled l1

--- b9-spy-game/Assets/Scripts/GameHandler_PauseMenu.cs
+++ b9-spy-game/Assets/Scripts/GameHandler_PauseMenu.cs
@@ -1,10 +1,11 @@
- using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
 
-public class GameHandler_PauseMenu : MonoBehaviour {
+public class GameHandler_PauseMenu : MonoBehaviour
+{
 
         public static bool GameisPaused = false;
         public GameObject pauseMenuUI;
@@ -12,49 +13,61 @@ public class GameHandler_PauseMenu : MonoBehaviour {
         public static float volumeLevel = 1.0f;
         private Slider sliderVolumeCtrl;
 
-        void Awake(){
+        void Awake()
+        {
                 pauseMenuUI.SetActive(true); // so slider can be set
-                SetLevel (volumeLevel);
+                SetLevel(volumeLevel);
                 GameObject sliderTemp = GameObject.FindWithTag("PauseMenuSlider");
-                if (sliderTemp != null){
+                if (sliderTemp != null)
+                {
                         sliderVolumeCtrl = sliderTemp.GetComponent<Slider>();
                         sliderVolumeCtrl.value = volumeLevel;
                 }
         }
 
-        void Start(){
+        void Start()
+        {
                 pauseMenuUI.SetActive(false);
                 GameisPaused = false;
         }
 
-        void Update(){
-                if (Input.GetKeyDown(KeyCode.Escape)){
-                        if (GameisPaused){
+        void Update()
+        {
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                        if (GameisPaused)
+                        {
                                 Resume();
                         }
-                        else{
+                        else
+                        {
                                 Pause();
                         }
                 }
         }
 
-        public void Pause(){
-              if (!GameisPaused){
-                pauseMenuUI.SetActive(true);
-                Time.timeScale = 0f;
-                GameisPaused = true;}
-             else{ Resume ();}
-             //NOTE: This added conditional is for a pause button
+        public void Pause()
+        {
+                if (!GameisPaused)
+                {
+                        pauseMenuUI.SetActive(true);
+                        Time.timeScale = 0f;
+                        GameisPaused = true;
+                }
+                else { Resume(); }
+                //NOTE: This added conditional is for a pause button
         }
 
-        public void Resume(){
+        public void Resume()
+        {
                 pauseMenuUI.SetActive(false);
                 Time.timeScale = 1f;
                 GameisPaused = false;
         }
 
-        public void SetLevel(float sliderValue){
-                mixer.SetFloat("MusicVolume", Mathf.Log10 (sliderValue) * 20);
+        public void SetLevel(float sliderValue)
+        {
+                mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
                 volumeLevel = sliderValue;
         }
 }
